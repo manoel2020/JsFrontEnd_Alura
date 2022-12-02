a2f7662fbfa5948bb0e8090a2150d094f22370e2
manoel2020 <junior.miranda2100@gmail.com>
2022-12-02 01:13:27 -0300

    *Refatoração Alura Midi
    Continuação do curso alura midi, aonde vai ser implementado a identificação de eventos do teclado no projeto.

--- Alura_Midi/Alura_Midi_Curso/main.js
+++ Alura_Midi/Alura_Midi_Curso/main.js
@@ -1,29 +1,38 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 /*
 	Obtem uma lista de elementos que possui a classe '.tecla',
 	no caso abaixo, pegou todos os botões.
 */
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let cont = 0;
+// let cont = 0;
 
-while (cont < listaDeTeclas.length) {
-    
-	/*
-		para não ficar repetindo listaTe...[cont], atribui o elemento da interação a uma variavel, 
-		que passa a ser manipulado atraves dessa variavel que recebeu o elemento atual do loop
-	*/
-    let tecla = listaDeTeclas[cont];
+// while (cont < listaDeTeclas.length) {
+//   /*
+// 		para não ficar repetindo listaTe...[cont], atribui o elemento da interação a uma variavel,
+// 		que passa a ser manipulado atraves dessa variavel que recebeu o elemento atual do loop
+// 	*/
+//   let tecla = listaDeTeclas[cont];
 
-    let classeSom = tecla.classList[1];
+//   let classeSom = tecla.classList[1];
 
-    let idAudio = `#som_${classeSom}`;
+//   let idAudio = `#som_${classeSom}`;
 
-    tecla.onclick = function () {
-        tocaSom(idAudio);
-    };
-    cont += 1;
-}
+//   tecla.onclick = function () {
+//     tocaSom(idAudio);
+//   };
+//   cont += 1;
+// }
+
+listaDeTeclas.forEach((tecla) => {
+  let classeSom = tecla.classList[1];
+
+  let idAudio = `#som_${classeSom}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+});
