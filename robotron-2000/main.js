1fc3a5f83f0b25fe19e1f697d9240aa4d1126702
manoel2020 <junior.miranda2100@gmail.com>
2022-12-23 01:13:15 -0300

    Refatoração.
    refatorado as variáveis para um mais fácil entendimento e comentado o projeto...

--- robotron-2000/main.js
+++ robotron-2000/main.js
@@ -1,17 +1,35 @@
-const controle = document.querySelectorAll("[data-controle]");
+/* 
+  Listar todos os btn que tem o DATA-CONTROLE 
+  O data-controle é uma propriedade que existe no botão aonde pode ser criado n identificadores.
+*/
+const btnMaisMenos = document.querySelectorAll("[data-controle]");
 
-controle.forEach((elemento) => {
-  elemento.addEventListener("click", (evento) => {
-    manipularDados(evento.target.dataset.controle, evento.target.parentNode);
+btnMaisMenos.forEach((btnUnico) => {
+  btnUnico.addEventListener("click", (evento) => {
+    /* 
+      evento é o proprio botão e todas as suas propriedades
+    */
+
+    // Pegar o valor Atribuido a propriedade Data-??? do btn
+    let valorPropriedadeDatadoBtn = evento.target.dataset.controle;
+
+    // retorna o elemento ancestral(Parent)
+    let ParentDoBtn = evento.target.parentNode;
+
+    manipularDados(valorPropriedadeDatadoBtn, ParentDoBtn);
   });
 });
 
-function manipularDados(operacao, controle) {
-  const pecas = controle.querySelector("[data-contador]");
+function manipularDados(ValorDaPropriedadeDatadoBtn, controle) {
+  // Atraves do parent é possivel pegar o input text especifico para manipular seu valor
+  const inputComPropriedadeDataContador =
+    controle.querySelector("[data-contador]");
 
-  if (operacao == "-") {
-    pecas.value = parseInt(pecas.value) - 1;
+  if (ValorDaPropriedadeDatadoBtn == "-") {
+    inputComPropriedadeDataContador.value =
+      parseInt(inputComPropriedadeDataContador.value) - 1;
   } else {
-    pecas.value = parseInt(pecas.value) + 1;
+    inputComPropriedadeDataContador.value =
+      parseInt(inputComPropriedadeDataContador.value) + 1;
   }
 }
